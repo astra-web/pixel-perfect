23d30eafcb4d7c7b776fb1668d42e6d751a72640
mauro-balades <mauro.balades@tutanota.com>
2023-04-20 20:24:59 +0200

    Created the unit conversion

--- src/dialog/insert.js
+++ src/dialog/insert.js
@@ -40,6 +40,20 @@ if (!document.getElementById("pixel-perfect-dialog")) {
                 })
             }
 
+            function convertUnit() {
+                let result = shadowRoot.getElementById("pixel-perfect--unit-result");
+                let unit1 = shadowRoot.getElementById("pixel-pefect--select-meassure-1");
+                let unit2 = shadowRoot.getElementById("pixel-pefect--select-meassure-2");
+                let input = shadowRoot.getElementById("pixel-perfect--unit-input");
+
+                result.value = this.convertUnit(input.value === "" ?
+                 0 : parseInt(input.value), unit1.options[unit1.selectedIndex].value, unit2.options[unit2.selectedIndex].value);
+            }
+
+            shadowRoot.getElementById("pixel-perfect--unit-input").addEventListener("input", convertUnit.bind(this));
+            shadowRoot.getElementById("pixel-pefect--select-meassure-1").addEventListener("change", convertUnit.bind(this));
+            shadowRoot.getElementById("pixel-pefect--select-meassure-2").addEventListener("change", convertUnit.bind(this));
+
             shadowRoot.getElementById("pixel-perfect--go-home").addEventListener("click", goHome.bind(this))
 
         }
@@ -57,6 +71,26 @@ if (!document.getElementById("pixel-perfect-dialog")) {
             }
             return matchingElements;
         }
+
+        convertUnit(value, fromUnit, toUnit) {
+            const unitFactors = {
+              'px': 1,
+              'pt': 1.3333333333333333,
+              'em': 16,
+              'rem': 16,
+              'vw': 0.01,
+              'vh': 0.01,
+              'vmin': 0.0104166666666667,
+              'vmax': 0.0104166666666667,
+              'in': 96,
+              'cm': 37.7952755905512,
+              'mm': 3.77952755905512,
+              'pc': 16,
+            };
+          
+            const pxValue = value * unitFactors[fromUnit];
+            return pxValue / unitFactors[toUnit];
+          }
 
         getAllElementsWithID(r, id) {
             var matchingElements = [];
