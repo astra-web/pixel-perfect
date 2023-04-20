02fff0fa4f44fdea5721eae7974d8ad937191f44
mauro-balades <mauro.balades@tutanota.com>
2023-04-20 18:53:14 +0200

    Added sliding menus

--- src/dialog/insert.js
+++ src/dialog/insert.js
@@ -6,13 +6,73 @@ if (!document.getElementById("pixel-perfect-dialog")) {
             super();
             const shadowRoot = this.attachShadow({mode: 'open'});
             const div = document.getElementById("--inner-pixel-perfect-component");
-            console.log(div)
             div.remove();
-    
+
+            let setSection = function (section) {
+                let id = `pixel-perfect-section--${section}`;
+                let wrapper = shadowRoot.getElementById("pixel-perfect--sections");
+
+                wrapper.classList.add("active");
+                shadowRoot.getElementById(id).classList.add("active");
+                shadowRoot.getElementById("pixel-perfect--home").classList.add("remove");
+                shadowRoot.getElementById("pixel-perfect--home").classList.remove("active");
+            };
+
+            let goHome = function () {
+                let sections = this.getAllElementsWithID(shadowRoot, "pixel-perfect-section--");
+                let wrapper = shadowRoot.getElementById("pixel-perfect--sections");
+
+                wrapper.classList.remove("active");
+                for (let i = 0; i < sections.length; i++) {
+                    sections[i].classList.remove("active");
+                }
+                shadowRoot.getElementById("pixel-perfect--home").classList.remove("remove");
+                shadowRoot.getElementById("pixel-perfect--home").classList.add("active");
+            };
+
             shadowRoot.appendChild(div);
+
+            let activators = this.getAllElementsWithAttribute(shadowRoot, 'data-pixel-perfect-section');
+            for (let i = 0; i < activators.length; i++) {
+                let activator = activators[i];
+                activator.addEventListener("click", () => {
+                    setSection(activator.getAttribute("data-pixel-perfect-section"));
+                })
+            }
+
+            shadowRoot.getElementById("pixel-perfect--go-home").addEventListener("click", goHome.bind(this))
+
+        }
+
+        getAllElementsWithAttribute(r, attribute) {
+            var matchingElements = [];
+            var allElements = r.querySelectorAll('*');
+            for (var i = 0, n = allElements.length; i < n; i++)
+            {
+                if (allElements[i].getAttribute(attribute) !== null)
+                {
+                    // Element exists with attribute. Add to array.
+                    matchingElements.push(allElements[i]);
+                }
+            }
+            return matchingElements;
+        }
+
+        getAllElementsWithID(r, id) {
+            var matchingElements = [];
+            var allElements = r.querySelectorAll('*');
+            for (var i = 0, n = allElements.length; i < n; i++)
+            {
+                if (allElements[i].id === id)
+                {
+                    // Element exists with attribute. Add to array.
+                    matchingElements.push(allElements[i]);
+                }
+            }
+            return matchingElements;
         }
     }
-    
+
     customElements.define('pixel-perfect-root', PixelPerfectRoot);
 
     fetch(chrome.runtime.getURL('src/dialog/index.html')).then(r => r.text()).then(html => {
