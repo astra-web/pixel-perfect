818de15b4e70ea0a032fad06b5c510d0eee4ec68
mauro-balades <mauro.balades@tutanota.com>
2024-01-16 19:49:55 +0100

    Fixed resize buttons again

--- src/background/index.js
+++ src/background/index.js
@@ -9,10 +9,10 @@ chrome.action.onClicked.addListener(function(tab) {
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request && request.action === "resizeWindow") {
+  if (request && request.type === "resizeWindow") {
     chrome.windows.getCurrent(function (window) {
       var updateInfo = { width: parseInt(request.width), height: parseInt(request.height) };
-      (updateInfo.state = "normal"), chrome.windows.update(window.id, updateInfo);
+      chrome.windows.update(window.id, updateInfo);
     });
   }
 });
